b93224157243cd6096a52cbc69614af01800aa45
Ivan Suiyama Silva <ivan.suiya@gmail.com>
2025-09-17 21:06:20 -0300

    Fix: corrijindo url do banco

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_z8TajBX9uoGD@ep-polished-rain-a8jwhmrn-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require',
+  connectionString: '',
   ssl: {
     rejectUnauthorized: false
   }
